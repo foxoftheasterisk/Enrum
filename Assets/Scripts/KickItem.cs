72f7fb4c444625e43d6de3490cd081f80aa0aeb3
JimmyTheHack <don'thackmypassword1530>
2016-12-11 14:00:34 -0500

    Added dictionary of Swedish names displayed in console.  Currently only named after kicking each object.

---
+++ Assets/Scripts/KickItem.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KickItem : MonoBehaviour {
+
+	public static int numKicked = 0;
+
+	void Start() {
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		
+
+		// Play a sound if the colliding objects had a big impact.		
+		Debug.Log("Kick");
+		string kickedObject; 
+
+
+		if (ImportNameFile.inverseProductCatalog.TryGetValue(collision.gameObject, out kickedObject)) {
+					
+		} else {
+
+			//assign a new name to object that is kicked
+			kickedObject = ImportNameFile.productNames [numKicked];
+
+			//add product name & id to product catalog
+			ImportNameFile.productCatalog.Add (kickedObject, collision.gameObject);
+			ImportNameFile.inverseProductCatalog.Add (collision.gameObject, kickedObject);
+
+			numKicked++;
+		}
+		Debug.Log (kickedObject);
+	}
+}
