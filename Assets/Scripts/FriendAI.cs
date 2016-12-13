e5f362c0c4fd3f3ade5cd16122234ac1f037a608
JimmyTheHack <don'thackmypassword1530>
2016-12-13 03:44:12 -0500

    Largely functional game - ready for level creation.  Added friend AI and score.

---
+++ Assets/Scripts/FriendAI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Makes Camera follow Main Character
+public class FriendAI : MonoBehaviour {
+
+	public GameObject leader; 
+	public float stepspeed;
+	Transform target;
+	Vector3 targetCoordinates;
+	Vector2 randomOffset;
+	public float strayDistance=1;
+
+	// Use this for initialization
+	void Start () {
+		SetNewTarget ();
+	}
+
+	// Update is called once per frame
+	void Update () {			
+	}
+
+	void SetNewTarget(){
+		//loop forever
+			
+		//follow the leader
+		target = leader.transform;
+		//add a random stray factor from the leader
+		Vector2 randomOffset = Random.insideUnitCircle;
+		targetCoordinates = new Vector3 (target.position.x + strayDistance * randomOffset.x, target.position.y + strayDistance * randomOffset.y, target.position.z);
+		iTween.MoveTo (gameObject, iTween.Hash ("position", targetCoordinates, "time", 2, "onComplete", "SetNewTarget"));
+
+		//wait specified time before calling again
+		//yield return new WaitForSeconds(2);
+	}
+	void OnTriggerEnter (Collider target) {
+		iTween.Stop ();
+		//do what you want with collision
+		if(target.tag == "Furniture"){
+			//etc, etc
+		}
+	}
+
+}
--- Assets/Scripts/KickItem.cs
+++ Assets/Scripts/KickItem.cs
@@ -5,17 +5,59 @@ using UnityEngine.UI;
 public class KickItem : MonoBehaviour {
 
 	public static int numKicked = 0;
-	Text text;
-	bool won=false;
+	Text text;		
+	Text savings;
+
+	static bool won;
 
 	void Start() {
+		won = false;
+		GameObject TextDisplayField = GameObject.Find("FoundItem");
+		text = TextDisplayField.GetComponent <Text> ();
+
+		GameObject MoneyDisplayField = GameObject.Find("Savings");
+		savings = MoneyDisplayField.GetComponent <Text> ();
 	}
 
-	void OnCollisionEnter2D(Collision2D collision)
-	{
-		
+	void BuyObject(GameObject kickedObject, string objectName){
+		double price = 10*Mathf.Ceil(Random.Range (1, 100))-.01;
+		text.text = "Bought a " + objectName +"\nfor only $" + price.ToString();
+		ShoppingList.bankAccount -= price;
+		savings.text = "Life Savings: $" + ShoppingList.bankAccount.ToString ();
+		if (ShoppingList.bankAccount<0){
+			savings.color = Color.red;
+		}
+		Destroy (kickedObject);
+	}
 
-		// Play a sound if the colliding objects had a big impact.		
+
+	//check to see if item is on shopping list and purchase as appropriate
+	void checkShoppingList(GameObject kickedObject, string objectName){
+		if (ShoppingList.myList.ContainsKey (objectName)) {
+			ShoppingList.myList [objectName] = true;
+			Debug.Log ("item found");
+			//buy item
+			BuyObject (kickedObject, objectName);
+
+			//check for Win Condition
+			if (!ShoppingList.myList.ContainsValue (false)) {
+				won = true;
+				Debug.Log ("Game Completed!");
+				text.text = "You've completed your shopping list!";
+			}
+		
+			//if your friend finds the item, buy anyways
+		} else if (gameObject.tag == "Friend") {
+			BuyObject (kickedObject, objectName);
+		} else {
+			text.text = "Found a " + objectName;
+		}
+
+	}
+
+	//triggers automatically on impact with object
+	void OnCollisionEnter2D(Collision2D collision)
+	{				
 		Debug.Log("Kick");
 		string objectName; 
 		GameObject kickedObject = collision.gameObject;
@@ -25,25 +67,10 @@ public class KickItem : MonoBehaviour {
 			if (ImportNameFile.inverseProductCatalog.TryGetValue (kickedObject, out objectName)) {
 				//print name of item
 				Debug.Log (objectName);
-
-				GameObject FoundItem = GameObject.Find("FoundItem");
-
-				if (!won) {
-					text = FoundItem.GetComponent <Text> ();
-					text.text = "Found a " + objectName;
+				if (!won) {					
+					//check if on shopping list
+					checkShoppingList (kickedObject, objectName);
 				}
-				//check if on shopping list
-				if(ShoppingList.myList.ContainsKey(objectName)){
-					ShoppingList.myList[objectName] = true;
-					Debug.Log("item found");
-					Destroy (kickedObject);
-					if (!ShoppingList.myList.ContainsValue(false)){
-						won = true;
-						Debug.Log("Game Completed!");
-						text.text = "You've completed your shopping list!";
-					}
-				}
-
 			} 
 		}
 
